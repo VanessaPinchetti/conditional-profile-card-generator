0553b419e981ecb91b7ba55a77fea06d11ec6bea
VanessaPinchetti <vanessa.pinchetti@gmail.com>
2022-01-03 18:41:38 +0000

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -55,7 +55,7 @@ function render(variables = {}) {
           <h1>${variables.name} ${variables.lastname}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
-          <ul class="position-right">
+          <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/VanePinchetti"${variables.twitter}><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/VanessaPinchetti"${variables.github}><i class="fab fa-github"></i></a></li>
             <li><a href="https://www.linkedin.com/in/vanessa-pinchetti-inostroza-a73712160/"${variables.linkedin}><i class="fab fa-linkedin"></i></a></li>
